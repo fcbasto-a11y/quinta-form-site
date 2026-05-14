d29afad6868e5d7b1f9e0e797fb8b60e0dc74d4f
fcbasto-a11y <fc.basto@gmail.com>
2026-05-14 12:35:49 +0800

    2nd deploy

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 //  Paste your Google Apps Script URL below after setup
 // ============================================================
 
-window.SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxKAmEPwmIoH3ohxfwzou9p5h8pD4a7gAv0n4Q1kZ7nfGFVbv4aqhvmM4nRwI7JvOcG/exec';
+window.SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzbUFVzxwAL4E689GPiLeqXJYl_wIWtJH88vI-CPHWKC4Eot_2NpzQW-LKxUKjdcXUK/exec';
